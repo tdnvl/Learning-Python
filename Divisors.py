9d66f094a9c40e085c15cd66ce518e35e5a0fda0
bummel1889@gmail.com <G1thub-1854>
2017-05-02 07:35:58 -0400

    Create a program that asks the user for a number and then prints out a list of all the divisors of that number.

---
+++ Divisors.py
@@ -0,0 +1,13 @@
+number = input("Give me a number... ")
+
+my_list = range(1, number+1)
+
+divisors = []
+
+for i in my_list:
+    if number % i == 0:
+        divisors.append(i)
+    else:
+        continue
+
+print ("The list of divisors is " + str(divisors))
