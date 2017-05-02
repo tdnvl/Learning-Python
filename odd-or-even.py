5930e4a3422b3258964be7f8b2860452214be59b
bummel1889@gmail.com <G1thub-1854>
2017-05-02 07:10:35 -0400

    First exercise: Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message. Check if the number is a multiple of 4, too.

---
+++ odd-or-even.py
@@ -0,0 +1,12 @@
+num = input("Give me a number! ")
+
+if num % 4 == 0:
+    print ("This is a multiple of 4")
+
+elif num % 2 == 0:
+    print("This is an even number")
+
+else:
+    print ("This is an odd number")
+
+print (num)
