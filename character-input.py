4bdb6d1958d2a2e458cabba885c56edad65b7b9f
tdnvl <bummel1889@gmail.com>
2017-08-08 07:05:01 -0400

    Completed extra assignment on exercise Character Input.

--- character-input.py
+++ character-input.py
@@ -1,6 +1,7 @@
 import datetime
 name = raw_input("What is your name? \n")
 age = raw_input("What is your age? \n")
+times = int(raw_input("How many times do you want to print this message? \n"))
 now = datetime.datetime.now()
 year = now.year + (100 - int(age))
-print(name + ", you will turn 100 in " + str(year))
+print(times * (name + ", you will turn 100 in " + str(year) + "\n"))
