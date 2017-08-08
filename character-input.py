a88dc6db12d7a0c4d7232072f58617cd0f988c80
tdnvl <bummel1889@gmail.com>
2017-08-07 21:42:52 -0400

    New exercise: Character input. To start again.

---
+++ character-input.py
@@ -0,0 +1,6 @@
+import datetime
+name = raw_input("What is your name? \n")
+age = raw_input("What is your age? \n")
+now = datetime.datetime.now()
+year = 100 - 
+print(name + ", you will turn 100 in " + year)
