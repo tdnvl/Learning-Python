efb60c4b3bbe61a59fcdbb13a831961d73f34026
tdnvl <bummel1889@gmail.com>
2017-08-11 08:00:23 -0400

    Implemented midpoint in palyndrome exercise.

--- String-lists.py
+++ String-lists.py
@@ -4,14 +4,21 @@ length = int(len(word))
 lengt = length - 1
 print(lengt)
 int(lengt)
-for l in range(0,lengt):
+
+if length % 2 == 0:
+    midpoint = length / 2
+else:
+    midpoint = (length / 2) +1
+
+
+for l in range(1,midpoint):
     # the end of the range should be the rounded-up length / 2 so it stops when it reaches the middle letter
     test = 0
     if word[l] == word[lengt-l]:
         test = test + 1
     else:
         print("Not a palyndrome")
-    if test == length:
+    if test == midpoint:
         print("This is a palyndrome")
     else:
         break
