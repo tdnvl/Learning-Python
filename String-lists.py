a48da02cc61b4cae8c04b8259114ad0c5ec49f8e
tdnvl <bummel1889@gmail.com>
2017-08-12 15:38:40 -0400

    Cleaned up the midpoint calculation.

--- String-lists.py
+++ String-lists.py
@@ -1,25 +1,31 @@
-word = str(raw_input ("Type a word... "))
-print(word)
+word = str(input("Type a word... "))
 length = int(len(word))
-lengt = length - 1
-print(lengt)
-int(lengt)
+print(length)
+length = length + 1
+test = 0
 
+
+# Let's define the index of the midpoint in word
 if length % 2 == 0:
     midpoint = length / 2
+    print ("The midpoint is " + str(midpoint) + "\n")
 else:
-    midpoint = (length / 2) +1
+    midpoint = (length / 2) + 1
+    print ("The midpoint is " + str(midpoint) + "\n")
 
 
+# Let's compare the first and the last characters, and move towards the midpoint
 for l in range(1,midpoint):
-    # the end of the range should be the rounded-up length / 2 so it stops when it reaches the middle letter
-    test = 0
-    if word[l] == word[lengt-l]:
-        test = test + 1
-    else:
+ last = length - l
+ print(word[last])
+ if word[l] == word[last]:
+    test = test + 1
+    print(test)
+ else:
         print("Not a palyndrome")
-    if test == midpoint:
-        print("This is a palyndrome")
-    else:
-        break
+
+if test == midpoint:
+    print("This is a palyndrome")
+else:
+    print ("This is not a palyndrome")
 
