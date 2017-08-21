54eb43e211aec09b5ebd75fdb0e131cf03ca812f
tdnvl <bummel1889@gmail.com>
2017-08-21 06:56:29 -0400

    9 cases covered. Added 'Play again.'

--- Rock-paper-scissors.py
+++ Rock-paper-scissors.py
@@ -1,30 +1,43 @@
-while True:
-    choice1 = str(raw_input("Player one, what's your move?\nType R for Rock, P for Paper, and S for Scissors\n>> "))
-    if choice1.lower() not in ('r', 'p', 's'):
-        print ("Not a valid choice.")
+play = True
+
+while play == True:
+
+    while True:
+        choice1 = str(raw_input("Player one, what's your move?\nType R for Rock, P for Paper, and S for Scissors\n>> "))
+        if choice1.lower() not in ('r', 'p', 's'):
+            print ("Not a valid choice.")
+        else:
+            break
+    while True:
+        choice2 = str(raw_input("Player two, what's your move?\nType R for Rock, P for Paper, and S for Scissors\n>> "))
+        if choice2.lower() not in ('r', 'p', 's'):
+            print ("Not a valid choice.")
+        else:
+            break
+
+    # I'm trying to have the name of the player in an array. That might or might not be useful, but I'm trying.
+    p1 = ["Player 1",choice1.lower()]
+    p2 = ["Player 2", choice2.lower()]
+
+    # first case: Same choice!
+    if p1[1] == p2[1]:
+        print ("Draw! Play again.")
+    elif p1[1] == 'r' and p2[1] == 's':
+        print (p1[0] + " wins! Rock beats scissors.")
+    elif p1[1] == 'r' and p2[1] == 'p':
+        print (p2[0] + " wins! Paper beats rock.")
+    elif p1[1] == 'p' and p2[1] == 's':
+        print (p2[0] + " wins! Scissors beats paper.")
+    elif p1[1] == 'p' and p2[1] == 'r':
+        print (p1[0] + " wins! Paper beats rock.")
+    elif p1[1] == 's' and p2[1] == 'r':
+        print (p2[0] + " wins! Rock beats scissors.")
+    elif p1[1] == 's' and p2[1] == 'p':
+        print (p1[0] + " wins! Scissors beats paper.")
+
+    again = raw_input("Do you want to play again?\nY or N\n>> ")
+    if again.lower() == "y":
+        play = True
     else:
-        break
-while True:
-    choice2 = str(raw_input("Player two, what's your move?\nType R for Rock, P for Paper, and S for Scissors\n>> "))
-    if choice2.lower() not in ('r', 'p', 's'):
-        print ("Not a valid choice.")
-    else:
-        break
-
-# I'm trying to have the name of the player in an array. That might or might not be useful, but I'm trying.
-p1 = ["Player 1",choice1.lower()]
-p2 = ["Player 2", choice2.lower()]
-
-# first case: Same choice!
-if p1[1] == p2[1]:
-    print ("Draw! Play again.")
-elif p1[1] == 'r' and p2[1] == 's':
-    print (p1[0] + " wins! Rock beats scissors.")
-elif p1[1] == 'r' and p2[1] == 'p':
-    print (p2[0] + " wins! Paper beats rock.")
-elif p1[1] == 'p' and p2[1] == 's':
-    print (p2[0] + " wins! Scissors beats paper.")
-elif p1[1] == 'p' and p2[1] == 's':
-    print (p2[0] + " wins! Scissors beats paper.")
-
+        play = False
 
