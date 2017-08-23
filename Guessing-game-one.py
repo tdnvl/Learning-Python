3f7c37dd6fb841c3c31147cf995ce017ac24bbbb
tdnvl <bummel1889@gmail.com>
2017-08-23 07:17:06 -0400

    Fixed the type error. Checking for exit before comparing integer value.

--- Guessing-game-one.py
+++ Guessing-game-one.py
@@ -3,8 +3,18 @@ a = random.randint(1, 9)
 game = True
 i = 0
 while game == True:
-    guess = int(raw_input("Guess a number between 1 an 9...\n>> "))
+    guess = raw_input("Guess a number between 1 an 9...\n>> ")
     i = i + 1
+
+    # First check for exit, while it's a string, to break
+
+    if guess == "exit":
+        break
+
+    # Once the exit has been checked, the other conditional tests will be with integers. Let's force the type for the rest of the script.
+
+    guess = int(guess)
+
     if guess == a:
         print ("Yay! You guessed it in " + str(i) + " guess(es)!")
         game = False
