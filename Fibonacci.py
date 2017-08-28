4798df21fd0649e119b47bfb592deb8a467980d8
tdnvl <bummel1889@gmail.com>
2017-08-27 22:13:02 -0400

    New idea for the Fibonacci exercise.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,6 @@
-num = input("How many Fibonacci numbers do you want to see?\n>> ")
+numbers = input("How many Fibonacci numbers do you want to see?\n>> ")
 
 
-def fibo(num):
-    for i in range(0, num):
-        series[0] = 1
-        series[1] = 1
-        
+def fibo(numbers):
+    series=[0,1] # I might as well start my series with the first two elements. It's pretty straightforward after that.
+
