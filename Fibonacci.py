4379d241ecc9c563eb716de005bb65d8348c2852
tdnvl <bummel1889@gmail.com>
2017-08-30 21:47:37 -0700

    Playing with the -1 and -2 index for a list to generate the latest element.

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,5 +2,15 @@ numbers = input("How many Fibonacci numbers do you want to see?\n>> ")
 
 
 def fibo(numbers):
-    series=[0,1] # I might as well start my series with the first two elements. It's pretty straightforward after that.
+    series=[1,1] # I might as well start my series with the first two elements.
+    if numbers == 1:
+        print("1")
+    elif numbers == 2:
+        print("1,1")
+    elif numbers > 2:
+        while len(series) < numbers:
+            series.append(series[-1] + series[-2])
+
+
+
 
