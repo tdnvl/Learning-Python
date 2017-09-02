bb2467ac37551f37caf72c83e5688678ba0a67ce
tdnvl <bummel1889@gmail.com>
2017-09-01 17:06:28 -0700

    Finished the second function. Dirty...

--- List-remove-duplicates.py
+++ List-remove-duplicates.py
@@ -2,17 +2,26 @@ def list_generator():
     import random
     random.seed(None,2)
     non_deduped_list = []
-    list_length = random.randint(1,99)
-    print(list_length)
+    # I'm liminting the length of the list to 30 elements, so it's more readable.
+    list_length = random.randint(1,30)
     for i in range(list_length):
         new_integer = random.randint(1,99)
         non_deduped_list.append(new_integer)
-    print(non_deduped_list)
+    return non_deduped_list
 
-list_generator()
+def deduping_list(long_list):
+    # set(long_list)
+    # return list(long_list)
+    print(sorted(list(set(long_list))))
 
 
-
+a = list_generator()
+print(a)
+print(sorted(a))
+# c = [1,1,2,2,3,3]
+# b = set(c)
+# print(list(b))
+deduping_list(a)
 
 
 
