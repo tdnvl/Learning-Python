51c0320095540ffd3626a34dd3233a9bb385d2c2
tdnvl <bummel1889@gmail.com>
2017-09-03 11:50:37 -0700

    The sentence splitting function is in place.

---
+++ Reverse-word-order.py
@@ -0,0 +1,10 @@
+# Ask for a phrase to revert
+phrase = str(input("Type a phrase to revert:\n>> "))
+
+# Split phrase into words
+def split_phrase(phrase):
+    words = phrase.split()
+    print(words)
+
+split_phrase(phrase)
+
