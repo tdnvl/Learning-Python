fde06e04a394a18a70a522d063125e441fa51c12
tdnvl <bummel1889@gmail.com>
2017-09-02 17:55:25 -0700

    Cleaned and commented the two functions.

--- List-remove-duplicates.py
+++ List-remove-duplicates.py
@@ -1,30 +1,21 @@
-def list_generator():
+def list_generator(): # Builds a list of 1-30 elements in range 1-99.
     import random
     random.seed(None,2)
     non_deduped_list = []
-    # I'm liminting the length of the list to 30 elements, so it's more readable.
+    # I'm arbitrarily limiting the length of the list to 30 elements.
     list_length = random.randint(1,30)
     for i in range(list_length):
+        # I'm arbitrarily limiting the range to 1-99.
         new_integer = random.randint(1,99)
         non_deduped_list.append(new_integer)
     return non_deduped_list
 
 def deduping_list(long_list):
-    # set(long_list)
-    # return list(long_list)
+    # This looks efficient, but not very good practice. I need to ask.
     print(sorted(list(set(long_list))))
 
 
 a = list_generator()
 print(a)
 print(sorted(a))
-# c = [1,1,2,2,3,3]
-# b = set(c)
-# print(list(b))
 deduping_list(a)
-
-
-
-# origin = [1,1,3,5,7,8,12,4,12,1,7]
-# deduped = set(origin)
-# print(list(deduped))
