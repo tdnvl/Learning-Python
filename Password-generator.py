1f1bcfa25757e9923547a8f5976b88ed33083f68
tdnvl <bummel1889@gmail.com>
2017-09-12 07:05:52 -0400

    Getting an index out of range error on second iteration only. To be fixed.

--- Password-generator.py
+++ Password-generator.py
@@ -1,23 +1,26 @@
 import random
 # List of 62 characters to pull from to generate a password
-character_list = ["a","b","c","d","-","=","!","@","#","$","%","&"]
-random.seed(None,2)
-
+# character_list = ["a","b","c","d","-","=","!","@","#","$","%","&"]
+random.seed(None, 2)
+characters = open('characters.txt','r')
 n = int(input("How many characters in your password?\n>> "))
 password = []
 number = 1
 while number <= n:
-    character_index = random.randint(0,11)
-    password.append(character_list[character_index])
+    character_index = random.randint(0, 64)
+    print(character_index)
+    selector = characters.read()
+    new_character = selector[character_index]
+    password.append(new_character)
+    print(password)
     number = number + 1
-
+characters.close()
 print("This is your password: " + "".join(password))
-characters = open('characters.txt','r')
-print("Reading from: " + characters.name)
-line = characters.read()
-chain = []
-chain.append(line[0])
-chain.append(line[1])
-chain.append(line[3])
-print(chain)
+
+# print("Reading from: " + characters.name)
+# line = characters.read()
+# chain = []
+# chain.append(line[0])
+# chain.append(line[2])
+# print("".join(chain))
 
