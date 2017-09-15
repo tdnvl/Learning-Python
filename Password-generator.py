6a058c7f9daeb08a7a663a28098ac66386a40229
tdnvl <bummel1889@gmail.com>
2017-09-15 07:57:56 -0400

    Still some index issues I need to troubleshoot.

--- Password-generator.py
+++ Password-generator.py
@@ -8,12 +8,15 @@ password = []
 number = 1
 while number <= n:
     character_index = random.randint(0, 64)
-    print(character_index)
+    print("Character index: " + str(character_index))
     selector = characters.read()
     new_character = selector[character_index]
+    print("New character: " + str(new_character))
     password.append(new_character)
-    print(password)
+    print("Password: " + str(password))
     number = number + 1
+    print("Number: " + str(number))
+    del new_character
 characters.close()
 print("This is your password: " + "".join(password))
 
