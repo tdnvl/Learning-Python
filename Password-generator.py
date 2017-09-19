c499bb9c3b8ada7fd6464408c7a0985c2cffd955
tdnvl <bummel1889@gmail.com>
2017-09-18 22:40:12 -0400

    Still debugging the out-of-range error.

--- Password-generator.py
+++ Password-generator.py
@@ -1,6 +1,4 @@
 import random
-# List of 62 characters to pull from to generate a password
-# character_list = ["a","b","c","d","-","=","!","@","#","$","%","&"]
 random.seed(None, 2)
 characters = open('characters.txt','r')
 n = int(input("How many characters in your password?\n>> "))
@@ -19,11 +17,4 @@ while number <= n:
     del new_character
 characters.close()
 print("This is your password: " + "".join(password))
-
-# print("Reading from: " + characters.name)
-# line = characters.read()
-# chain = []
-# chain.append(line[0])
-# chain.append(line[2])
-# print("".join(chain))
 
