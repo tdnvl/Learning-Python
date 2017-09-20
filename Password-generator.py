2058b76d2b01ce4b6f2d07f4a814632262060dd5
tdnvl <bummel1889@gmail.com>
2017-09-20 13:47:42 -0400

    Fixed the index error. I didn't unsertand how the read() method worked.

--- Password-generator.py
+++ Password-generator.py
@@ -4,16 +4,12 @@ characters = open('characters.txt','r')
 n = int(input("How many characters in your password?\n>> "))
 password = []
 number = 1
+selector = characters.read()
 while number <= n:
     character_index = random.randint(0, 64)
-    print("Character index: " + str(character_index))
-    selector = characters.read()
     new_character = selector[character_index]
-    print("New character: " + str(new_character))
     password.append(new_character)
-    print("Password: " + str(password))
     number = number + 1
-    print("Number: " + str(number))
     del new_character
 characters.close()
 print("This is your password: " + "".join(password))
