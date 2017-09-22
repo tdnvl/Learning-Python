2f5982ef4b567e2082de18d02400a48d5f26d8bd
tdnvl <bummel1889@gmail.com>
2017-09-22 07:58:46 -0400

    Imported requests and BeautifulSoup successfully.

--- Crawl-a-webpage-with-beautifulsoup.py
+++ Crawl-a-webpage-with-beautifulsoup.py
@@ -1,6 +1,10 @@
 import requests
-
 url = 'http://github.com'
 r = requests.get(url)
 r_html = r.text
-print(r_html)
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(r_html)
+title = soup.find('span', 'articletitle').string
+
+
