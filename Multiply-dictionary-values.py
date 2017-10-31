b872c9cba0cc59a7a02acb2cb615319f116cd29f
tdnvl <bummel1889@gmail.com>
2017-10-31 15:22:06 -0500

    Complete multiply dictionary values.

--- Multiply-dictionary-values.py
+++ Multiply-dictionary-values.py
@@ -1,6 +1,7 @@
 # Ex 11 at https://www.w3resource.com/python-exercises/dictionary
 # Write a Python program to multiply all the items in a dictionary
 dic1 = {1: 12, 2: 45, 3: -12, 9: 22, 102: 11}
-dic_product = {}
+dic_product = 1
 for i in dic1.values():
-    dic_product =
+    dic_product = dic_product * i
+print(dic_product)
