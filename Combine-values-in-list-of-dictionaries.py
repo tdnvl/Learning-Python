2262a2d0b2545d9b1ae71db8a99da3c799bbc8c7
tdnvl <bummel1889@gmail.com>
2017-11-10 17:18:27 -0500

    Complete combine values exercise.

--- Combine-values-in-list-of-dictionaries.py
+++ Combine-values-in-list-of-dictionaries.py
@@ -6,17 +6,14 @@ dct = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'ite
 # I can't iterate on dictionaries!
 # I want to extract each item in the list and add them to a Counter
 
-for i in range(0,len(dct),1):
-    dct1 = {}
-    dct2 = {}
-    dct1 = dct[i]
-    print(dct1.values(),dct1.values())
-    C = Counter() # initiate Counter as empty
-    C = Counter(dct[i]) # create counter from each dictionary in the list
-    print(C)
+C = Counter() # initiate Counter as empty
 
-
-
-
+for l in dct:
+    D = Counter()
+    item = l['item']
+    amount = l['amount']
+    D[item] = amount
+    C.update(D)
+print(C)
 
 # Expected Output: Counter({'item1': 1150, 'item2': 300})
