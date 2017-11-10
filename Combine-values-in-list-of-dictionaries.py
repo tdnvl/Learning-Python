0be7937c99036a1c631dcdd90af3f6da88aee5c9
tdnvl <bummel1889@gmail.com>
2017-11-09 23:11:03 -0500

    Iterate in list and use a sum of counters.

--- Combine-values-in-list-of-dictionaries.py
+++ Combine-values-in-list-of-dictionaries.py
@@ -1,21 +1,25 @@
 # Ex 23. Write a Python program to combine values in python list of dictionaries.
 from collections import Counter
 
-items = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
-counted_items = Counter(items)
-print(counted_items)
-
-for i in items:
+dct = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
 
 
+D = Counter()
+E = Counter() # Do I need three counters to do this?
 
-A = [1, 2, 3, 3, 4, -1, 0, 2, 2, 2, 3]
-C = Counter(A) # Creates a Counter object that counts how many times
-               # you have seen each value
-print(C)
+# I can't iterate on dictionaries!
+# I want to extract each item in the list and add them to a Counter
+
+for i in range(0,len(dct),1):
+    C = Counter() # initiate Counter as empty
+    C = Counter(dct[i]) # create counter from each dictionary in the list
+    E = C + D # add Counter C to Counter D
+    D = E # Before looping,
+
+print(E)
+
+
+
+
 
 # Expected Output: Counter({'item1': 1150, 'item2': 300})
-
-E = 'aaabbbccdefadsfsvas'
-F = Counter(E)
-print(F)
