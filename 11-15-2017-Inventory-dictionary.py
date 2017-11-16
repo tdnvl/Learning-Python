c9f894b66c7e3f0c4d6ad102ac97b0dd70e410a5
tdnvl <bummel1889@gmail.com>
2017-11-15 22:10:33 -0500

    Start inventory dictionary exercise.

---
+++ 11-15-2017-Inventory-dictionary.py
@@ -0,0 +1,6 @@
+# Exercise 1 at https://erlerobotics.gitbooks.io/erle-robotics-learning-python-gitbook-free/lists/exercises_list_and_dictionaries.html
+inventory = {'gold' : 500,'pouch' : ['flint', 'twine', 'gemstone'],'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']}
+inventory['pocket'] = ['seashell','strange berry','lint']
+print(inventory)
+
+# .sort()the items in the list stored under the 'backpack' key
