b0c71c1bc7c06c96c8173858c02478c43ca977a9
tdnvl <bummel1889@gmail.com>
2017-11-20 07:21:06 -0500

    Complete exercise price and inventory.

---
+++ 2017-11-20-groceries-price-inventory.py
@@ -0,0 +1,8 @@
+# Exercise 2 at https://erlerobotics.gitbooks.io/erle-robotics-learning-python-gitbook-free/lists/exercises_list_and_dictionaries.html
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+inventory = {"banana": 10, "apple": 8, "orange": 3, "pear": 5}
+for i in prices:
+    print(i)
+    print("price: ", prices[i])
+    print("inventory: ", inventory[i])
+    print("__________\n")
