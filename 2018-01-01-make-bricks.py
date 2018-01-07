e3775e96f4f6a0d52a7a6140171d6bd2b6c7954d
tdnvl <bummel1889@gmail.com>
2018-01-07 16:32:28 -0500

    Update the makebrick exercise file.

--- 2018-01-01-make-bricks.py
+++ 2018-01-01-make-bricks.py
@@ -1,10 +1,31 @@
 def make_bricks(small, big, goal):
-    bigg = 5*big
-    if bigg != 0:
-        remainder = goal % bigg
-    # goal made of small bricks only
-    if small != 0 and 1 >= goal >= small:
-        return True
-    elif small != 0 and bigg != 0 and 1 >= remainder >= small:
-        return True
+    a = goal / 5
+    # if a >= 1 we can use at least one big brick
+    # if a < 1 we cannot use a big brick
+    b = goal % 5
+    # if b == 0 the goal is a multiple of 5
+    # if b != 0 we need to check if we have enough small bricks
+    # to reach the goal
+    c = int(a)
+    d = c*big + b*small
 
+    if b == 0:
+        if big >= a:
+            return True
+        elif big < a and ((a - big) * 5) <= small:
+            return True
+        elif big < a and (a - big)*5 > small:
+            return False
+        elif goal > small:
+            return False
+        elif goal <= small:
+            return True
+    elif b != 0:
+      if big < a and small >= b:
+        return True
+      elif big < a and small < b:
+        return False
+    elif small == 0 and b == 0 and big >= a:
+      return True
+    elif small == 0 and b == 0 and big < a:
+      return False
